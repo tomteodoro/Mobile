a87050ab16c6eeffb9fa3860aa8561d1bafafbb7
Tom Teodoro <wellintoncteodoro@gmail.com>
2026-02-26 22:50:53 -0300

    debug

--- Flutter/hello_world/lib/main.dart
+++ Flutter/hello_world/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            'Hot?...',
+            'Oi',
             style: TextStyle(fontSize: 30),
           ),
         ),
